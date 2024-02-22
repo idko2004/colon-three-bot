e03a6d4a92759791d5cbb854fd595d13c6ace362
idko2004 <ivancito72011@gmail.com>
2024-02-22 17:39:38 -0300

    error by one :sob:

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const config = require('./config.json');
 
 let emojis = ['F', ':D', '>:3', ':3', 'uwu', 'unu', 'owo', 'UwU', 'UnU', 'OwO', ':o', ':c', ':v', ':p', ':/', 'c:', ':)'];
 
+console.log(new Date().toString());
+
 const client = new Discord.Client(
 {
 	ws:
@@ -36,32 +38,65 @@ client.on(Discord.Events.ClientReady, async (e) =>
 
 client.on(Discord.Events.MessageCreate, async (msg) =>
 {
-	if(msg.author.bot) return;
-
-	const msgSplitted = msg.content.split(' ');
-	for(let i = 0; i < msgSplitted.length; i++)
+	try
 	{
-		if(emojis.includes(msgSplitted[i]))
+		if(msg.author.bot) return;
+
+		const msgSplitted = msg.content.split(' ');
+		for(let i = 0; i < msgSplitted.length; i++)
 		{
-			msg.channel.send(msgSplitted[i]);
-			return;
+			if(emojis.includes(msgSplitted[i]))
+			{
+				msg.channel.send(msgSplitted[i]);
+				return;
+			}
 		}
+	}
+	catch(err)
+	{
+		console.error('\n--HA OCURRIDO UN ERROR-- (MessageCreate)\n', err);
 	}
 });
 
 async function sendMessagesAtStartup()
 {
-	const random = require('./random');
-
-	let channel;
-	let emoji;
-	for(let i = 0; i < config.MESSAGE_AT_STARTUP.length; i++)
+	try
 	{
-		channel = await client.channels.fetch(config.MESSAGE_AT_STARTUP[i]);
-		emoji = emojis[random.range(0, emojis.length)];
-		console.log(emoji);
-		await channel.send({content: emoji});
+		const random = require('./random');
+
+		let channel;
+		let emoji;
+		for(let i = 0; i < config.MESSAGE_AT_STARTUP.length; i++)
+		{
+			channel = await client.channels.fetch(config.MESSAGE_AT_STARTUP[i]);
+			emoji = emojis[random.range(0, emojis.length - 1)];
+			console.log(emoji);
+			await channel.send({content: emoji});
+		}
+	}
+	catch(err)
+	{
+		console.error('\n--HA OCURRIDO UN ERROR-- (sendMessagesAtStartup)\n', err);
 	}
 }
 
-client.login(config.DISCORD_TOKEN);
+
+async function start()
+{
+	let tryAgain = true;
+	while(tryAgain)
+	{
+		try
+		{
+			await client.login(config.DISCORD_TOKEN);
+			tryAgain = false;
+		}
+		catch(err)
+		{
+			console.error('\n--HA OCURRIDO UN ERROR-- (start)\n', err);
+			tryAgain = true;
+		}
+	}
+}
+
+start();
