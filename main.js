e594b414b516a200311be16e4c2a90382b268d61
idko2004 <ivancito72011@gmail.com>
2024-04-27 12:45:49 -0400

    nuevo sistema de detección de emojis, ahora debería detectar casos como :DDDDD

--- main.js
+++ main.js
@@ -2,19 +2,12 @@ const Discord = require('discord.js');
 
 const config = require('./config.json');
 
-let emojis = ['F', ':D', '>:3', ':3', 'uwu', 'unu', 'owo', 'twt', 'UwU', 'UnU', 'OwO', 'TwT', ':o', ':O', ':0', ':c', ':v', ':V', ':p', ':P', ':/', 'c:', ':)', ':(', ":'(", ":'c", ':>', ':<'];
+let emojis = [':D', '>:3', ':3', 'D:', 'c:', ':)', ':(', ":'(", 'uwu', 'unu', 'owo', 'twt', 'UwU', 'UnU', 'OwO', 'TwT', ':o', ':O', ':0', ':c', ':v', ':V', ':p', ':P', ':/', ':\\', ':|', ":'c", ':>', ':<', 'F'];
 
 console.log(new Date().toString());
 
 const client = new Discord.Client(
 {
-	ws:
-	{
-		properties:
-		{
-			$browser: "Discord Android"
-		}
-	},
 	intents:
 	[
         Discord.GatewayIntentBits.Guilds,
@@ -27,6 +20,9 @@ const client = new Discord.Client(
 		Discord.Partials.Channel
 	]
 });
+
+//Poner que el cliente es en realidad Discord de Android, no sé si a discord le guste esto pero es que el bot está corriendo en un móvil con android, no puedo no poner que el icono de que está conectado sea un móvil
+Discord.DefaultWebSocketManagerOptions.identifyProperties.browser = 'Discord Android';
 
 client.on(Discord.Events.ClientReady, async (e) =>
 {
@@ -45,11 +41,11 @@ client.on(Discord.Events.MessageCreate, async (msg) =>
 		const msgSplitted = msg.content.split(' ');
 		for(let i = 0; i < msgSplitted.length; i++)
 		{
-			if(emojis.includes(msgSplitted[i]))
-			{
-				msg.channel.send(msgSplitted[i]);
-				return;
-			}
+			let toReply = textContainsEmoji(msgSplitted[i]);
+			
+			if(toReply === null) return;
+
+			msg.channel.send(toReply);
 		}
 	}
 	catch(err)
@@ -57,6 +53,51 @@ client.on(Discord.Events.MessageCreate, async (msg) =>
 		console.error('\n--HA OCURRIDO UN ERROR-- (MessageCreate)\n', err);
 	}
 });
+
+function textContainsEmoji(text)
+{
+	let validEmoji = true;
+
+	for(let i = 0; i < emojis.length; i++) //Iterar sobre todos los emojis
+	{
+		if(text.length < emojis[i]) continue; //Si el texto es más corto que el emoji, no hay forma de que sea ese emoji
+
+		validEmoji = false;
+		for(let iChar = 0; iChar < text.length; iChar++) //Iterar sobre los caracteres del texto
+		{
+			if(iChar >= emojis[i].length)
+			{
+				//Si el trozo de mensaje es mas largo que el emoji, pero los caracteres sobrantes son el mismo que el último caracter del emoji, sigue siendo un emoji válido. Ej: :DDDDDDDDDDDDDD
+				if(text[iChar] === emojis[i].at(-1))
+				{
+					validEmoji = true;
+					continue;
+				}
+				else
+				{
+					validEmoji = false;
+					break;
+				}
+			}
+
+			if(text[iChar] !== emojis[i][iChar])
+			{
+				validEmoji = false;
+				break;
+			}
+
+			validEmoji = true;
+		}
+
+		if(validEmoji)
+		{
+			console.log('Returning emoji', emojis[i]);
+			return emojis[i];
+		}
+	}
+
+	return null;
+}
 
 async function sendMessagesAtStartup()
 {
